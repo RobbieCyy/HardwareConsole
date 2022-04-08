5f02726d677f0b1f0dcfa4700606909c99586512
Guo Weixuan <cnmozzie@gmail.com>
2022-04-08 10:59:06 +0800

    <fix>(ccd): 修复了小尺寸图片显示问题
    - 后期需要重新修正一下图片的方向

--- CCDConsole/datahandler.cpp
+++ CCDConsole/datahandler.cpp
@@ -30,34 +30,35 @@ void DataHandler::generateImageData(unsigned short *data, bool uploadData) {
     int min = 65535;
     int max = 0;
     QByteArray encode_data;
-    for (int i=0; i<width; i++)
-        for(int j=0; j<height; j++) {
-            if(data[i*height+j]<min) {
-                min = data[i*height+j];
-            }
-            if(data[i*height+j]>max) {
-                max = data[i*height+j];
-            }
-            temp = (data[i*height+j]-lowCount) * 256 / (highCount-lowCount);
-            if(temp < 0) {
-                temp = 0;
-            }
-            else if(temp > 255) {
-                temp = 255;
-            }
-            m_imageData[i*height+j] = temp;
-            if (uploadData) {
-                char a = (data[i*height+j] >> 8) & 0xff;
-                char b = data[i*height+j] & 0xff;
-                encode_data.append(a);
-                encode_data.append(b);
-            }
+    for (int i=0; i<512*512; i++)
+        m_imageData[i] = 0;
+    for (int i=0; i<width*height; i++) {
+        if(data[i]<min) {
+            min = data[i];
         }
+        if(data[i]>max) {
+            max = data[i];
+        }
+        temp = (data[i]-lowCount) * 256 / (highCount-lowCount);
+        if(temp < 0) {
+            temp = 0;
+        }
+        else if(temp > 255) {
+            temp = 255;
+        }
+        m_imageData[((i%width)+m_ccd->hstart()-1)*512+(i/width+m_ccd->vstart()-1)] = temp;
+        if (uploadData) {
+            char a = (data[i] >> 8) & 0xff;
+            char b = data[i] & 0xff;
+            encode_data.append(a);
+            encode_data.append(b);
+        }
+    }
     if(m_autoScale) {
         lowCount = min;
         highCount = max;
     }
-    m_imageProvider->generateImage(m_imageData, width, height);
+    m_imageProvider->generateImage(m_imageData, 512, 512);
     if (hasServer && uploadData) {
         m_server->sendData(encode_data);
     }
